5d8b41ab3f16d4247e7df1eb4cce20e48daa96ff
KotsiubniakVadym <vad.kocubniak@gmail.com>
2025-11-11 18:13:01 +0200

    Added project

---
+++ Chicken_Eggs_Game/Chicken_Eggs_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chicken_Eggs_GameApp.swift
+//  Chicken_Eggs_Game
+//
+//  Created by Vadim Koc on 11.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Chicken_Eggs_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chicken_Eggs_GameTests/Chicken_Eggs_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chicken_Eggs_GameTests.swift
+//  Chicken_Eggs_GameTests
+//
+//  Created by Vadim Koc on 11.11.2025.
+//
+
+import Testing
+@testable import Chicken_Eggs_Game
+
+struct Chicken_Eggs_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
